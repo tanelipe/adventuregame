96485ef6510efdd9c8c08aa5f2017adfb4745dfc
Nina Ranta <nina.ranta@edu.pkamk.fi>
2013-01-12 14:21:49 +0200

    Commit

--- adventuregame/engine/Gold.h
+++ adventuregame/engine/Gold.h
@@ -8,16 +8,33 @@
 #define __Gold_h__
 ////////////////////////////////////////////////////////////////////////////////
 class GoldFactory;
+
+//--- Nina Ranta ---
+class Game;
+//-------
+
 ////////////////////////////////////////////////////////////////////////////////
 class Gold 
 {
   friend class GoldFactory;
+  // ---- Nina Ranta -----
+  friend class Game;
 private:
   unsigned int m_nAmount;
+  // ---- Nina Ranta ----  
+  Gold();
   Gold(unsigned int value);
+  unsigned int c_nAmount;
+  // ----
+
 public:
   virtual ~Gold();
   unsigned int GetAmount() const;
+  // ---- Nina Ranta -----
+  unsigned int GetCountAmount() const;
+  void SetCountAmount( unsigned int count );
+  void ZeroCountAmount( unsigned int count);
+  // ----
   void SetAmount( unsigned int value );
 };
 ////////////////////////////////////////////////////////////////////////////////
